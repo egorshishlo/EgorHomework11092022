c968d7bdab7efe1697a0cb61db2219ffa89550cd
egorshishlo <e.shishlo@yandex.by>
2022-08-11 15:23:45 +0300

    Initial Commit

---
+++ EgorHomework11092022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EgorHomework11092022
+//
+//  Created by Егор Шишло on 11.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
